a121d120421990e1e41efbb70e09412a238dc0f3
Woojun Choi <tim.woojune1@gmail.com>
2018-06-14 23:47:25 -0700

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -19,36 +19,36 @@ var banner = ['/*!\n',
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function() {
 
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+  // // Bootstrap
+  // gulp.src([
+  //     './node_modules/bootstrap/dist/**/*',
+  //     '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+  //     '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  //   ])
+  //   .pipe(gulp.dest('./vendor/bootstrap'))
 
-  // Font Awesome
-  gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
-    ])
-    .pipe(gulp.dest('./vendor/font-awesome'))
+  // // Font Awesome
+  // gulp.src([
+  //     './node_modules/font-awesome/**/*',
+  //     '!./node_modules/font-awesome/{less,less/*}',
+  //     '!./node_modules/font-awesome/{scss,scss/*}',
+  //     '!./node_modules/font-awesome/.*',
+  //     '!./node_modules/font-awesome/*.{txt,json,md}'
+  //   ])
+  //   .pipe(gulp.dest('./vendor/font-awesome'))
 
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
+  // // jQuery
+  // gulp.src([
+  //     './node_modules/jquery/dist/*',
+  //     '!./node_modules/jquery/dist/core.js'
+  //   ])
+  //   .pipe(gulp.dest('./vendor/jquery'))
 
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+  // // jQuery Easing
+  // gulp.src([
+  //     './node_modules/jquery.easing/*.js'
+  //   ])
+  //   .pipe(gulp.dest('./vendor/jquery-easing'))
 
 });
 
